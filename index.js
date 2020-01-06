240ddaf68fda3f947ce2ccbec82496abc3a3a45f
trlanfeng <trlanfeng@foxmail.com>
2020-01-06 12:16:50 +0800

    有那味了

--- index.js
+++ index.js
@@ -2,42 +2,68 @@ const canvas = document.getElementById('canvas');
 canvas.width = 500;
 canvas.height = 100;
 const ctx = canvas.getContext('2d');
-const log_button = document.getElementById('log');
+let originImageData;
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+
 
 function main() {
   var image = new Image();
   image.onload = function () {
-    console.log('TR: image.onload -> image', image.width);
     ctx.drawImage(image, 0, 0);
+    originImageData = ctx.getImageData(0, 0, 500, 100);
   }
-  image.src = './color.png';
-  log_button.addEventListener('click', log);
+  image.src = './google.png';
+  button1.addEventListener('click', button1_click);
+  button2.addEventListener('click', button2_click);
 }
 
-function log() {
-  let y = 0;
-  for (let i = 0, len = 30; i < len; i += 5) {
-    y++;
-    offset(10 + y, i);
+function button1_click() {
+  requestAnimationFrame(animation);
+  // ctx.save();
+  // for (let i = 0, len = 30; i < len; i++) {
+  //   const x = Math.round(Math.random() * 100) - 50;
+  //   offset(i + 30, x);
+  // }
+}
+
+function button2_click() {
+  // ctx.restore();
+}
+
+function animation() {
+  ctx.restore();
+  for (let i = 0, len = 30; i < len; i++) {
+    const x = Math.round(Math.random() * 20) - 10;
+    offset(i + 30, x);
   }
-  y = 0;
-  for (let i = 0, len = 30; i < len; i += 2) {
-    y++;
-    offset(5 + y, i);
-  }
+  requestAnimationFrame(animation);
+}
+
+function getLineData(y) {
+  return originImageData.data.slice(y * 500 * 4, (y + 1) * 500 * 4);
 }
 
 function offset(y, offset_x) {
   var newArray = new Uint8ClampedArray(500 * 4);
-  var imageData = ctx.getImageData(0, y, 500, 1);
-  console.log('TR: log -> imageData', imageData);
-  // 左5
-  newArray.set(imageData.data.slice(offset_x * 4));
-  newArray.fill(0, (500 - offset_x) * 4);
-  console.log('TR: log -> newArray', newArray);
-  var newImageData = new ImageData(newArray, 500, 1);
-  console.log('TR: log -> newImageData', newImageData);
-  ctx.putImageData(newImageData, 0, y);
+  var lineData = getLineData(y);
+  if (offset_x > 0) {
+    // 向右偏移
+    const x = Math.abs(offset_x);
+    newArray.set(lineData.slice(0, (500 - x) * 4), x * 4);
+    // 空位补0
+    newArray.fill(0, 0, x * 4);
+    var newLineData = new ImageData(newArray, 500, 1);
+    ctx.putImageData(newLineData, 0, y);
+  } else {
+    // 向左偏移
+    const x = Math.abs(offset_x);
+    newArray.set(lineData.slice(x * 4));
+    // 空位补0
+    newArray.fill(0, (500 - x) * 4);
+    var newLineData = new ImageData(newArray, 500, 1);
+    ctx.putImageData(newLineData, 0, y);
+  }
 }
 
 main();
