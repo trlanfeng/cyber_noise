7134b6642aa30e07c49c56a950f36fc7a006f181
trlanfeng <trlanfeng@foxmail.com>
2020-01-06 12:46:44 +0800

    适应不同大小图片

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 const canvas = document.getElementById('canvas');
-canvas.width = 500;
-canvas.height = 100;
 const ctx = canvas.getContext('2d');
 let originImageData;
+let image_width;
+let image_height;
 const button1 = document.getElementById('button1');
 const button2 = document.getElementById('button2');
 
+let isAnimate = false;
 
 function main() {
   var image = new Image();
   image.onload = function () {
+    image_width = image.width;
+    image_height = image.height;
+    canvas.width = image.width;
+    canvas.height = image.height;
+    canvas.style.width = image_width;
+    canvas.style.height = image_height;
     ctx.drawImage(image, 0, 0);
-    originImageData = ctx.getImageData(0, 0, 500, 100);
+    originImageData = ctx.getImageData(0, 0, image_width, image_height);
   }
-  image.src = './small.png';
-  button1.addEventListener('click', button1_click);
-  button2.addEventListener('click', button2_click);
+  image.src = './cyberpunk.jpg';
+  canvas.addEventListener('mouseover', button1_click);
+  canvas.addEventListener('mouseout', button2_click);
+  requestAnimationFrame(animation);
 }
 
 function button1_click() {
-  requestAnimationFrame(animation);
+  isAnimate = true;
+  console.log('TR: functionbutton1_click -> isAnimate', isAnimate);
+  // requestAnimationFrame(animation);
   // ctx.save();
   // for (let i = 0, len = 30; i < len; i++) {
   //   const x = Math.round(Math.random() * 100) - 50;
@@ -28,40 +38,43 @@ function button1_click() {
 }
 
 function button2_click() {
+  isAnimate = false;
+  ctx.putImageData(originImageData,0,0);
   // ctx.restore();
 }
 
 function animation() {
-  ctx.restore();
-  for (let i = 0, len = 30; i < len; i++) {
-    const x = Math.round(Math.random() * 20) - 10;
-    offset(i + 30, x);
+  if (isAnimate) {
+    for (let i = 0, len = image_height / 5; i < len; i += 1) {
+      const x = Math.round(Math.random() * 20) - 10;
+      offset(i + image_height * 0.4, x);
+    }
   }
   requestAnimationFrame(animation);
 }
 
 function getLineData(y) {
-  return originImageData.data.slice(y * 500 * 4, (y + 1) * 500 * 4);
+  return originImageData.data.slice(y * image_width * 4, (y + 1) * image_width * 4);
 }
 
 function offset(y, offset_x) {
-  var newArray = new Uint8ClampedArray(500 * 4);
+  var newArray = new Uint8ClampedArray(image_width * 4);
   var lineData = getLineData(y);
   if (offset_x > 0) {
     // 向右偏移
     const x = Math.abs(offset_x);
-    newArray.set(lineData.slice(0, (500 - x) * 4), x * 4);
+    newArray.set(lineData.slice(0, (image_width - x) * 4), x * 4);
     // 空位补0
     newArray.fill(0, 0, x * 4);
-    var newLineData = new ImageData(newArray, 500, 1);
+    var newLineData = new ImageData(newArray, image_width, 1);
     ctx.putImageData(newLineData, 0, y);
   } else {
     // 向左偏移
     const x = Math.abs(offset_x);
     newArray.set(lineData.slice(x * 4));
     // 空位补0
-    newArray.fill(0, (500 - x) * 4);
-    var newLineData = new ImageData(newArray, 500, 1);
+    newArray.fill(255, (image_width - x) * 4);
+    var newLineData = new ImageData(newArray, image_width, 1);
     ctx.putImageData(newLineData, 0, y);
   }
 }
